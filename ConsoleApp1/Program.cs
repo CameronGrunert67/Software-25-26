94995580948907f40bdebb93e3b324c1eb4bf3ad
Cameron Grunert <camerongrunert@gmail.com>
2025-08-05 08:12:07 -0700

    What is Your name

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!!");
+String name = "Cameron Grunert";
+Console.WriteLine("Hello " + name);
+Console.WriteLine($"Hello {name}");
+Console.WriteLine("What is your first name?");
+string firstName = Console.ReadLine();
+Console.WriteLine($"Nice to meet you, {firstName}!");
+
+Console.WriteLine("What is your last name?");
+string lastName = Console.ReadLine();
+Console.WriteLine($"Nice to meet you, {lastName}!");
+
+Console.WriteLine("What grade are you in?");
+string grade = Console.ReadLine();
+Console.WriteLine($"You are in grade {grade}.");
+
+Console.WriteLine("What is your student ID?");
+string studentId = Console.ReadLine();
+Console.WriteLine($"Your student ID is {studentId}.");
